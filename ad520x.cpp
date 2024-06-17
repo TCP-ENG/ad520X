4654b9366e868dd1dcb021103272766f9ec00a1d
TCP-ENG <129853844+Travis-DM@users.noreply.github.com>
2024-06-17 12:05:10 -0400

    Added function to take in 16bit data from SPI message

--- ad520x.cpp
+++ ad520x.cpp
@@ -11,3 +11,10 @@ void setAD5204(uint8_t cs, uint8_t chan, uint8_t value)
     SPI.endTransaction();
     digitalWrite(cs,HIGH);
 }
+
+void setAD5204(uint8_t cs, uint16_t data)
+{
+    ad520xPKT pkt;
+    pkt.d16 = data;
+    setAD5204(cs,pkt.b.channel,pkt.b.value);
+}
--- ad520x.h
+++ ad520x.h
@@ -2,8 +2,23 @@
 #define _AD520X_H_
 #include <Arduino.h>
 
+#define PACKED __attribute__ ((packed))
+typedef union
+{
+    uint16_t d16;
+    /* data */
+    struct 
+    {
+        uint8_t channel;
+        uint8_t value;  
+    } b;
+}ad520xPKT;
+
+
+
 
 void setAD5204(uint8_t cs, uint8_t chan, uint8_t value);
+void setAD5204(uint8_t cs, uint16_t data);
 
 
 #endif
